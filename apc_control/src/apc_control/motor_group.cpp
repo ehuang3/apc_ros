125b265cb0a30a4006bedf7e672edb43836c6309
Eric Huang <ehuang@gatech.edu>
2015-05-10 23:10:08 -0400

    control: Fix read state in precondition to use non-stupid timeout

--- apc_control/src/apc_control/motor_group.cpp
+++ apc_control/src/apc_control/motor_group.cpp
@@ -653,7 +653,7 @@ namespace apc_control
         }
 
         // Read in state with timeout.
-        if (ret = readState(true))
+        if (ret = readState(100))
             return ret;
 
         // Wait for at most 0.1 seconds for us to stop moving.
