2d63d6c64990da092a35d6da0b0901999a08d594
jpanikulam <jpanikulam@ufl.edu>
2015-05-16 19:44:19 -0400

    Vision: Add support for multi-object simultaneous segmentation

--- apc_pcl/matsrc/learning/apc_get_divergences.m
+++ apc_pcl/matsrc/learning/apc_get_divergences.m
@@ -1,9 +1,11 @@
 function [divergences] = get_divergences(distributions_1, distributions_2, method)
+    % get_divergences(distributions_1, distributions_2, method)
+    % Compute the divergences between distributions 1 and 2 using $method
     if nargin < 3
         method = 'js';
     end
-    divergences = zeros(size(distributions_1, 1));
+    divergences = zeros(size(distributions_1, 1), 1);
     for k = 1:size(distributions_1, 1)
-        divergences(k) = apc_divergence(distributions_1(1, :), distributions_2(1, :), method);
+        divergences(k) = apc_divergence(distributions_1(k, :), distributions_2(k, :), method);
     end
 end
--- apc_pcl/matsrc/visualization/apc_crop_histogram.m
+++ apc_pcl/matsrc/visualization/apc_crop_histogram.m
@@ -1,6 +1,8 @@
-function [crop_histogram] = apc_crop_histogram(image)
+function [distributions] = apc_crop_histogram(image)
     figure, imshow(image)
     [x, y, cropped, rect] = imcrop(image);
+    % if isequal(x, [])
+
     distributions = apc_get_distributions(cropped);
     apc_show_histogram(distributions);
 end
