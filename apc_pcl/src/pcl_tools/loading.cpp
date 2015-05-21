3d35bbdd6c02b185af61d35c5b9e5bcbd5b2fc02
jpanikulam <jpanikulam@ufl.edu>
2015-05-20 22:05:26 -0400

    PCL: Add minor features to support many seed ICP

--- apc_pcl/src/pcl_tools/loading.cpp
+++ apc_pcl/src/pcl_tools/loading.cpp
@@ -6,6 +6,12 @@
 #include <pcl/point_types.h>
 #include <pcl/io/vtk_lib_io.h>
 #include <pcl/PCLPointCloud2.h>
+#include <vtkPolyData.h>
+#include <vtkPLYReader.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+
 #include "pcl_tools.h"
 
 namespace pcl_tools {
@@ -18,10 +24,14 @@ namespace pcl_tools {
     }
 
     void cloud_from_ply(std::string filename, pcl::PointCloud<pcl::PointXYZRGBA>& cloud) {
-        /* Extract a pcl::PointCloudXYZ from a .ply file*/
-        pcl::PolygonMesh mesh;
-        pcl::io::loadPolygonFilePLY(filename, mesh);
-        pcl::fromPCLPointCloud2(mesh.cloud, cloud);
+        /* Extract a pcl::PointCloudXYZ from a .ply file 
+            Code courtesy of Nikolaus Mitchell */
+        vtkSmartPointer<vtkPLYReader> reader = vtkSmartPointer<vtkPLYReader>::New();
+        vtkSmartPointer<vtkPolyData> data = vtkSmartPointer<vtkPolyData>::New();
+        reader->SetFileName (filename.c_str());
+        reader->Update();
+        data = reader->GetOutput();
+        pcl::io::vtkPolyDataToPointCloud(data, cloud);
     }
 
     bool cloud_from_pcd(std::string filename, pcl::PointCloud<pcl::PointXYZ>& cloud) {
